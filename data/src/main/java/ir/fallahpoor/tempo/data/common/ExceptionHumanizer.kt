3b9e8f6481f04c7583d85e940e29bbb98b9bd672
masood <masood.fallahpoor@gmail.com>
2019-09-14 10:41:59 +0430

    Refactor ExceptionHumanizer

--- data/src/main/java/ir/fallahpoor/tempo/data/common/ExceptionHumanizer.kt
+++ data/src/main/java/ir/fallahpoor/tempo/data/common/ExceptionHumanizer.kt
@@ -2,19 +2,16 @@ package ir.fallahpoor.tempo.data.common
 
 import java.io.IOException
 
-class ExceptionHumanizer private constructor() {
+object ExceptionHumanizer {
 
-    companion object {
-        private const val NO_INTERNET_CONNECTION = "No Internet connection"
-        private const val SOMETHING_WENT_WRONG = "Something went wrong"
+    const val NO_INTERNET_CONNECTION = "No Internet connection"
+    const val SOMETHING_WENT_WRONG = "Something went wrong"
 
-        fun getHumanizedErrorMessage(t: Throwable): String {
-            return when (t) {
-                is IOException -> NO_INTERNET_CONNECTION
-                else -> SOMETHING_WENT_WRONG
-            }
+    fun getHumanizedErrorMessage(t: Throwable): String {
+        return when (t) {
+            is IOException -> NO_INTERNET_CONNECTION
+            else -> SOMETHING_WENT_WRONG
         }
-
     }
 
 }
