2f45ded2eddd496b58748e44a58a5584760e1609
masood <masood.fallahpoor@gmail.com>
2019-10-30 12:34:05 +0330

    Refactor DataSource classes

--- data/src/main/java/ir/fallahpoor/tempo/data/datasource/category/CategoriesDataSource.kt
+++ data/src/main/java/ir/fallahpoor/tempo/data/datasource/category/CategoriesDataSource.kt
@@ -22,26 +22,27 @@ class CategoriesDataSource
 
         setState(State.LOADING)
 
-        try {
-            val response: Response<CategoriesEnvelop> =
-                categoriesWebService.getCategories(offset = 0, limit = params.pageSize)
-                    .execute()
+        val state =
+            try {
 
-            val state = if (response.isSuccessful) {
-                val categoriesEntity = response.body()!!.categoriesEntity
-                callback.onResult(categoriesEntity.items, 0, categoriesEntity.total)
-                State.LOADED
-            } else {
-                State(State.Status.ERROR, response.message())
+                val response: Response<CategoriesEnvelop> =
+                    categoriesWebService.getCategories(offset = 0, limit = params.pageSize)
+                        .execute()
+
+                if (response.isSuccessful) {
+                    val categoriesEntity = response.body()!!.categoriesEntity
+                    callback.onResult(categoriesEntity.items, 0, categoriesEntity.total)
+                    State.LOADED
+                } else {
+                    State(State.Status.ERROR, response.message())
+                }
+
+            } catch (ex: Exception) {
+                val message = ExceptionHumanizer.getHumanizedErrorMessage(ex)
+                State(State.Status.ERROR, message)
             }
 
-            setState(state)
-
-        } catch (ex: Exception) {
-            val message = ExceptionHumanizer.getHumanizedErrorMessage(ex)
-            val state = State(State.Status.ERROR, message)
-            setState(state)
-        }
+        setState(state)
 
     }
 
@@ -49,26 +50,26 @@ class CategoriesDataSource
 
         setState(State.LOADING_MORE)
 
-        try {
-            val response: Response<CategoriesEnvelop> = categoriesWebService.getCategories(
-                offset = params.startPosition,
-                limit = params.loadSize
-            ).execute()
+        val state =
+            try {
+                val response: Response<CategoriesEnvelop> = categoriesWebService.getCategories(
+                    offset = params.startPosition,
+                    limit = params.loadSize
+                ).execute()
 
-            val state: State = if (response.isSuccessful) {
-                callback.onResult(response.body()!!.categoriesEntity.items)
-                State.LOADED
-            } else {
-                State(State.Status.ERROR_MORE, response.message())
+                if (response.isSuccessful) {
+                    callback.onResult(response.body()!!.categoriesEntity.items)
+                    State.LOADED
+                } else {
+                    State(State.Status.ERROR_MORE, response.message())
+                }
+
+            } catch (ex: Exception) {
+                val message = ExceptionHumanizer.getHumanizedErrorMessage(ex)
+                State(State.Status.ERROR_MORE, message)
             }
 
-            setState(state)
-
-        } catch (ex: Exception) {
-            val message = ExceptionHumanizer.getHumanizedErrorMessage(ex)
-            val state = State(State.Status.ERROR_MORE, message)
-            setState(state)
-        }
+        setState(state)
 
     }
 
--- data/src/main/java/ir/fallahpoor/tempo/data/datasource/playlist/PlaylistsDataSource.kt
+++ data/src/main/java/ir/fallahpoor/tempo/data/datasource/playlist/PlaylistsDataSource.kt
@@ -22,30 +22,30 @@ class PlaylistsDataSource
 
         setState(State.LOADING)
 
-        try {
-            val response: Response<PlaylistsEnvelop> =
-                categoriesWebService.getPlaylists(
-                    categoryId,
-                    offset = 0,
-                    limit = params.pageSize
-                )
-                    .execute()
+        val state =
+            try {
+                val response: Response<PlaylistsEnvelop> =
+                    categoriesWebService.getPlaylists(
+                        categoryId,
+                        offset = 0,
+                        limit = params.pageSize
+                    )
+                        .execute()
 
-            val state = if (response.isSuccessful) {
-                val playlistsEntity = response.body()!!.playlistsEntity
-                callback.onResult(playlistsEntity.items, 0, playlistsEntity.total)
-                State.LOADED
-            } else {
-                State(State.Status.ERROR, response.message())
+                if (response.isSuccessful) {
+                    val playlistsEntity = response.body()!!.playlistsEntity
+                    callback.onResult(playlistsEntity.items, 0, playlistsEntity.total)
+                    State.LOADED
+                } else {
+                    State(State.Status.ERROR, response.message())
+                }
+
+            } catch (ex: Exception) {
+                val message = ExceptionHumanizer.getHumanizedErrorMessage(ex)
+                State(State.Status.ERROR, message)
             }
 
-            setState(state)
-
-        } catch (ex: Exception) {
-            val message = ExceptionHumanizer.getHumanizedErrorMessage(ex)
-            val state = State(State.Status.ERROR, message)
-            setState(state)
-        }
+        setState(state)
 
     }
 
@@ -53,27 +53,28 @@ class PlaylistsDataSource
 
         setState(State.LOADING_MORE)
 
-        try {
-            val response: Response<PlaylistsEnvelop> = categoriesWebService.getPlaylists(
-                categoryId,
-                offset = params.startPosition,
-                limit = params.loadSize
-            ).execute()
+        val state =
+            try {
 
-            val state: State = if (response.isSuccessful) {
-                callback.onResult(response.body()!!.playlistsEntity.items)
-                State.LOADED
-            } else {
-                State(State.Status.ERROR_MORE, response.message())
+                val response: Response<PlaylistsEnvelop> = categoriesWebService.getPlaylists(
+                    categoryId,
+                    offset = params.startPosition,
+                    limit = params.loadSize
+                ).execute()
+
+                if (response.isSuccessful) {
+                    callback.onResult(response.body()!!.playlistsEntity.items)
+                    State.LOADED
+                } else {
+                    State(State.Status.ERROR_MORE, response.message())
+                }
+
+            } catch (ex: Exception) {
+                val message = ExceptionHumanizer.getHumanizedErrorMessage(ex)
+                State(State.Status.ERROR_MORE, message)
             }
 
-            setState(state)
-
-        } catch (ex: Exception) {
-            val message = ExceptionHumanizer.getHumanizedErrorMessage(ex)
-            val state = State(State.Status.ERROR_MORE, message)
-            setState(state)
-        }
+        setState(state)
 
     }
 
