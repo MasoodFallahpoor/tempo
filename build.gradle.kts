b8f38d9019a3225fb7a564b5d71032f5711129a1
masood <masood.fallahpoor@gmail.com>
2020-02-09 11:44:35 +0330

    Update Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         maven("https://dl.bintray.com/kotlin/kotlin-eap")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.0.0-alpha06")
+        classpath("com.android.tools.build:gradle:4.0.0-alpha09")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.2.0-rc03")
     }
