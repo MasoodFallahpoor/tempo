7f2961a2c11c75b6017fb223dc71b8757d7128da
masood <masood.fallahpoor@gmail.com>
2020-06-13 10:13:23 +0430

    Do not fetch categories on config change

--- app/src/main/java/ir/fallahpoor/tempo/categories/view/CategoriesFragment.kt
+++ app/src/main/java/ir/fallahpoor/tempo/categories/view/CategoriesFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -73,7 +72,7 @@ class CategoriesFragment : Fragment() {
             addItemDecoration(SpaceItemDecoration(requireContext(), 8f, 2))
             addOnScrollListener(object : EndlessScrollListener() {
                 override fun onLoadMore() {
-                    categoriesViewModel.getCategories()
+                    categoriesViewModel.getMoreCategories()
                 }
             })
         }
--- app/src/main/java/ir/fallahpoor/tempo/categories/viewmodel/CategoriesViewModel.kt
+++ app/src/main/java/ir/fallahpoor/tempo/categories/viewmodel/CategoriesViewModel.kt
@@ -2,9 +2,11 @@ package ir.fallahpoor.tempo.categories.viewmodel
 
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
-import ir.fallahpoor.tempo.common.*
+import ir.fallahpoor.tempo.common.BaseViewModel
+import ir.fallahpoor.tempo.common.DataLoadedState
+import ir.fallahpoor.tempo.common.ErrorState
+import ir.fallahpoor.tempo.common.LoadingState
 import ir.fallahpoor.tempo.data.common.ExceptionHumanizer
-import ir.fallahpoor.tempo.data.entity.category.CategoriesEnvelop
 import ir.fallahpoor.tempo.data.entity.category.CategoryEntity
 import ir.fallahpoor.tempo.data.entity.common.ListEntity
 import ir.fallahpoor.tempo.data.repository.category.CategoriesRepository
@@ -19,32 +21,44 @@ class CategoriesViewModel
         const val LIMIT = 20
     }
 
+    private var categories = mutableListOf<CategoryEntity>()
     private var totalItems = Int.MAX_VALUE
     private var offset = 0
 
     fun getCategories() {
-
-        if (moreCategoriesAvailable()) {
-
-            setViewState(LoadingState())
-
-            val d: Disposable = categoriesRepository.getCategories(offset, LIMIT)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                    { c: ListEntity<CategoryEntity> ->
-                        totalItems = c.total
-                        offset += LIMIT
-                        setViewState(DataLoadedState(c.items))
-                    },
-                    { t: Throwable ->
-                        val message: String = ExceptionHumanizer.getHumanizedErrorMessage(t)
-                        setViewState(ErrorState(message))
-                    }
-                )
-
-            addDisposable(d)
-
+        if (categories.isEmpty()) {
+            _getCategories()
+        } else {
+            setViewState(DataLoadedState(categories))
         }
+    }
+
+    fun getMoreCategories() {
+        if (moreCategoriesAvailable()) {
+            _getCategories()
+        }
+    }
+
+    private fun _getCategories() {
+
+        setViewState(LoadingState())
+
+        val d: Disposable = categoriesRepository.getCategories(offset, LIMIT)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { c: ListEntity<CategoryEntity> ->
+                    categories.addAll(c.items)
+                    totalItems = c.total
+                    offset += LIMIT
+                    setViewState(DataLoadedState(categories))
+                },
+                { t: Throwable ->
+                    val message: String = ExceptionHumanizer.getHumanizedErrorMessage(t)
+                    setViewState(ErrorState(message))
+                }
+            )
+
+        addDisposable(d)
 
     }
 
