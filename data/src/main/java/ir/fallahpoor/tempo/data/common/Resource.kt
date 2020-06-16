c8d4cdd0ff42a7c7d8c5bba58fe2b8a6af831af3
masood <masood.fallahpoor@gmail.com>
2020-06-16 22:41:27 +0430

    Remove some unused classes

--- data/src/main/java/ir/fallahpoor/tempo/data/common/Resource.kt
+++
@@ -1,8 +0,0 @@
-package ir.fallahpoor.tempo.data.common
-
-sealed class Resource<T> {
-
-    class Success<T>(val data: T) : Resource<T>()
-    class Error<T>(val errorMessage: String) : Resource<T>()
-
-}
